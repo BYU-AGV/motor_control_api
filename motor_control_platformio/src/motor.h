c61509ff6a34b99e38ce2c101742eb20352579c2
mxc42 <morgandavidhunter@gmail.com>
2019-11-16 10:55:49 -0700

    updates

--- motor_control_platformio/src/motor.h
+++ motor_control_platformio/src/motor.h
@@ -11,18 +11,25 @@ class motor {
     uint8_t pwm_pin;
     uint8_t ena_pin;
     uint8_t dir_pin;
+
     bool is_left;
 
     uint16_t speed;
     bool direction;
+    bool enabled;
 
     void write_values();
 
    public:
     motor(uint8_t pwm_pin, uint8_t ena_pin, uint8_t dir_pin, bool is_left);
-    void set_speed(uint16_t speed, bool direction);
+    void set_speed(uint16_t speed);
     void enable();
     void disable();
+    uint8_t get_speed();
+    bool get_direction();
+    void inc_speed();
+    void dec_speed();
+    void set_direction(bool direction);
 };
 
 #endif  // MOTOR_H
