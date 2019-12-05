5a42e1f0e243052e4a1e4ae593bdf0365c1a2c0b
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2019-12-05 14:51:09 -0700

    updated motor controller
    Added forward, backward direction for motion controls
    Inlined small functions
    Fleshed out state machine a bit

--- motor_control_API/motorController.cpp
+++ motor_control_API/motorController.cpp
@@ -1,7 +1,7 @@
 #include "motorController.h"
 
 #define SM_DEBUG_ON 1	//used to turn debug statements on/off
-#define MAX_SPEED_FPS 7.333
+#define MAX_SPEED_FPS 7.333	//5 mph
 
 //constructor
 motorController::motorController(motor leftMotor, motor rightMotor, smPeriod) :
@@ -18,11 +18,10 @@ motorController::motorController(motor leftMotor, motor rightMotor, smPeriod) :
 
 //destructor
 motorController::~motorController()
-{
-}
+{}
 
 //drive robot in a straight line for a certain distance
-void motorController::straightLine(mc_speed_t speed, mc_distance_t distance)
+void motorController::straightLine(mc_speed_t speed, mc_distance_t distance, mc_FBDir_t direction)
 {
 	this->targetSpeed = speed;
 	this->targetDistance = distance;
@@ -30,7 +29,8 @@ void motorController::straightLine(mc_speed_t speed, mc_distance_t distance)
 
 //drive robot in an arc of a certain radius for a certain distance
 //use turnRadius 0 for car turn, ie. using one wheel
-void motorController::turnAtRadius(mc_LRDir_t direction, mc_distance_t turnRadius, mc_speed_t speed, mc_distance_t distance)
+void motorController::turnAtRadius(mc_LRDir_t LRdir, mc_FBDir_t FBdir,
+		mc_distance_t turnRadius, mc_speed_t speed, mc_distance_t distance)
 {
 }
 
@@ -42,10 +42,13 @@ void motorController::turnInPlace(mc_LRDir_t direction, mc_rotation_t degrees)
 }
 
 //used to find if robot is occupied. If false, robot is free for a command
-bool motorController::isAvailable()
-{
-	return isAvailable;
-}
+inline bool motorController::isAvailable() { return isAvailable; }
+
+//sets instruction recieved flag
+inline void motorController::instRecieved() { instRecievedFlag = true; }
+
+//lowers gameController flag
+inline void motorController::stopGameController() { gameControllerFlag = false; }
 
 //Gets actual left motor speed from encoders
 mc_speed_t motorController::getLeftSpeed()
@@ -67,6 +70,30 @@ mc_distance_t motorController::getRightDistance()
 {
 }
 
+//verifies that current speed matches desired speed, adjusts if necessary (PID)
+void motorController::speedCheck()
+{
+	
+}
+
+//checks if robot has traversed target distance, sets instruction completed flag
+inline void motorController::distCheck()
+{
+	if(average(getLeftDistance(), getRightDistance()) >= targetDistance)
+	{
+		instCompleted = true;
+	}
+}
+
+//helper functino to stop robot
+void motorController::stopRobot()
+{
+	leftMotor.disable();
+	rightMotor.disable();
+	leftMotor.set_speed(0);
+	rightMotor.set_speed(0);
+}
+
 //standard state machine tick function
 void motorController::tick()
 {
@@ -75,20 +102,128 @@ void motorController::tick()
 	{
 		switch(currState)
 		{
-			
+			case init_st:
+			{
+				if(prevState != currState)
+				{
+					printf("currentState = init_st");
+					prevState = currState;
+				}
+			}
+			break;
+			case getInstruction_st:
+			{
+				if(prevState != currState)
+				{
+					printf("currentState = getInstruction_st");
+					prevState = currState;
+				}
+			}
+			break;
+			case executingInstruction_st:
+			{
+				if(prevState != currState)
+				{
+					printf("currentState = executingInstruction_st");
+					prevState = currState;
+				}
+			}
+			break;
+			case gameController_st:
+			{
+				if(prevState != currState)
+				{
+					printf("currentState = gameController_st");
+					prevState = currState;
+				}
+			}
+			break;
 		}
 	}
 	
 	//advance state
 	switch(currState)
 	{
+		case init_st:
+		{
+			currState = getInstructions;
+			instRecievedFlag = false;
+			isAvailable = true;
+		}
+		break;
+		case getInstruction_st:
+		{
+			if(instRecievedFlag)
+			{
+				switch(nextInstruction)
+				{
+					case straightLine_inst:
+					{
+						currState = executingInstruction_st;
+					}
+					break;
+					case turnRad_inst:
+					{
+						currState = executingInstruction_st;
+					}
+					break;
+					case turnInPlace_inst:
+					{
+						currState = executingInstruction_st;
+					}
+					break;
+					case gameControl_inst:
+					{
+						currState = gamecontroller_st;
+					}
+					break;
+				}
+			}
+		}
+		break;
+		case executingInstruction_st:
+		{
+			if(instComplete)
+			{
+				stopRobot();
+				currState = getInstructions_st;
+				isAvailable = true;
+				instRecievedFlag = false;
+				instComplete = false;
+			}
+		}
+		break;
+		case gameController_st:
+		{
+			if(!gameControllerFlag)
+			{
+				currState = getInstruction_st;
+				isAvailable = true;
+				instRecievedFlag = false;
+			}
+		}
+		break;
 		
 	}
 	
-	//state advance
+	//state action
 	switch(currState)
 	{
-		
+		case init_st:
+		break;
+		case getInstruction_st:
+		break;
+		case executingInstruction_st:
+		{
+			distCheck(); 
+			speedCheck();
+		}
+		break;
+		case gameController_st:
+		{
+		}
+		break;
+
 	}
 	
 }
