40962cd6f265cdac8162b7fab99aa166edf055a2
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2019-12-07 12:27:05 -0700

    Skeletonized control functions

--- motor_control_API/motorController.cpp
+++ motor_control_API/motorController.cpp
@@ -4,15 +4,7 @@
 #define SM_DEBUG_ON 1	//used to turn debug statements on/off
 
 #define BAUD_RATE 9600
-
 #define MAX_SPEED_FPS 7.333	//5 mph
-
-#define INSTRUCTION_FUNC_MASK
-#define INSTRUCTION_SPEED_MASK
-#define INSTRUCTION_DIST_MASK
-#define INSTRUCTION_RADIUS_MASK
-#define INSTRUCTION_FB_DIR_MASK
-#define INSTRUCTION_LR_DIR_MASK
 
 //robot dimensions
 #define ROBOT_TRACK_WIDTH_IN		//distance from center of one wheel to the other in inches
@@ -27,7 +19,7 @@
 //macro to convert real speed in feet per second to PWM value
 #define FPStoPWM(speedIn) (speedIn * PWM_FACTOR)
 
-//macro to do the math to convert
+//macro to do the math to determine speed of inside/outside wheels while turning
 #define velCalc(speedIn, radiusIn) (2 * M_PI * speedIn * radiusIn)
 
 //constructor
@@ -45,20 +37,15 @@ motorController::motorController(motor leftMotor, motor rightMotor, smPeriod) :
 	//setup
 	
 	//init sercom slave for instructions from main controller
+	SERCOM I2CSlave(SERCOM0);
 	I2CSlave.initSlaveWIRE(MC_SLAVE_ADDRESS);
-	
 
 	//init master i2c for communication with encoders
+	SERCOM I2CMaster(SERCOM1);
 	I2CMaster.initMasterWIRE(BAUD_RATE);
 	
-	
-	//game controller for direct control
+	//game controller for direct control of robot
 	gameController.init();
-	
-	/*
-	uint8_t currInstruction = I2CSlave.readDataWIRE();
-	I2CMaster.sendDataMasterWIRE(NEED_SPEED_INST);
-	*/
 }
 
 //destructor
@@ -78,6 +65,7 @@ void motorController::updateMotors()
 //drive robot in a straight line for a certain distance
 void motorController::straightLine(mc_speed_t speed, mc_distance_t distance, mc_FBDir_t direction)
 {
+	//basic. Sets speeds and direction, updates motors
 	leftTargetSpeed = speed;
 	rightTargetSpeed = speed;
 	targetDistance = distance;
@@ -92,11 +80,13 @@ void motorController::straightLine(mc_speed_t speed, mc_distance_t distance, mc_
 void motorController::turnAtRadius(mc_LRDir_t LRdir, mc_FBDir_t FBdir,
 		mc_distance_t turnRadius, mc_speed_t speed, mc_distance_t distance)
 {
+	//basic. calculates inside and outside target speeds, updates motors depending on turn direction
 	turnDir = LRdir;
-	leftDir = (rightDir = FBdir);
+	leftDir = FBdir
+	rightDir = FBdir;
 	
-	mc_speed_t insideTargetSpeed = velCalc(speed, turnRadius - WHEEL_DIAMETER_IN/2),
-				outsideTargetSpeed = velCalc(speed, turnRadius + WHEEL_DIAMETER_IN/2);
+	mc_speed_t insideTargetSpeed = velCalc(speed, turnRadius - ROBOT_TRACK_WIDTH_IN/2),
+				outsideTargetSpeed = velCalc(speed, turnRadius + ROBOT_TRACK_WIDTH_IN/2);
 				
 	leftTargetSpeed = (LRdir == LEFT) ? insideTargetSpeed : outsideTargetSpeed;
 	rightTargetSpeed = (LRdir == LEFT) ? outsideTargetSpeed : insideTargetSpeed;
@@ -119,35 +109,12 @@ inline bool motorController::isAvailable() { return isAvailable; }
 //sets instruction received flag
 void motorController::getInstruction()
 {
-	uint8_t instructionStr = I2CSlave.readDataWIRE();
-	instruction_e funcCall = instructionStr & INSTRUCTION_FUNC_MASK;
-	switch(funcCall)
-	{
-		case straightLine_inst:
-		{
-			instSpeed = (instructionStr & INSTRUCTION_SPEED_MASK);
-			instDistance = (instructionStr & INSTRUCTION_DIST_MASK);
-			instFBDir = (instructionStr & INSTRUCTION_FB_DIR_MASK);			
-		}
-		break;
-		case turnRad_inst:
-		{
-			instSpeed = (instructionStr & INSTRUCTION_SPEED_MASK);
-			instDistance = (instructionStr & INSTRUCTION_DIST_MASK);
-			instRadius = (instructionStr & INSTRUCTION_RADIUS_MASK);
-			instFBDir = (instructionStr & INSTRUCTION_FB_DIR_MASK);
-			instLRDir = (instructionStr & INSTRUCTION_LR_DIR_MASK);
-		}
-		break;
-		case turnInPlace_inst:
-		break;
-		case gameControl_inst:
-		break;
-	}
+	nextInstruction = I2CSlave.readDataWIRE();		//FIXME - needs implementation
+	
 	instRecievedFlag = true;
 }
 
-//lowers gameController flag
+//lowers gameController flag (after exit button is pressed on controller)
 inline void motorController::stopGameController() { gameControllerFlag = false; }
 
 //Gets actual left motor speed from encoders
@@ -160,15 +127,12 @@ mc_speed_t motorController::getRightSpeed()
 {
 }
 
-//verifies that current speed matches desired speed, adjusts if necessary (PID)
+//verifies that current speed matches desired speed, adjusts if necessary (PI controller)
 void motorController::speedCheck()
 {
-	if(leftTargetSpeed != getLeftSpeed() ||
-	   rightTargetSpeed != getRightSpeed())
-	{
-		
-	}
+
 }
+
 //get distance left wheel has traveled from encoders
 mc_distance_t motorController::getLeftDistance()
 {
@@ -182,10 +146,6 @@ mc_distance_t motorController::getRightDistance()
 //checks if robot has traversed target distance, sets instruction completed flag
 inline void motorController::distCheck()
 {
-	if(avg(getLeftDistance(), getRightDistance()) >= targetDistance)
-	{
-		instComplete = true;
-	}
 }
 
 //helper function to stop robot
@@ -200,7 +160,7 @@ void motorController::stopRobot()
 //standard state machine tick function
 void motorController::tick()
 {
-	//debug SM
+	//State machine debugger
 	if(SM_DEBUG_ON)
 	{
 		switch(currState)
@@ -258,29 +218,7 @@ void motorController::tick()
 		{
 			if(instRecievedFlag)
 			{
-				switch(nextInstruction)
-				{
-					case straightLine_inst:
-					{
-						currState = executingInstruction_st;
-					}
-					break;
-					case turnRad_inst:
-					{
-						currState = executingInstruction_st;
-					}
-					break;
-					case turnInPlace_inst:
-					{
-						currState = executingInstruction_st;
-					}
-					break;
-					case gameControl_inst:
-					{
-						currState = gameController_st;
-					}
-					break;
-				}
+									//FIXME - Needs implementation
 			}
 		}
 		break;
@@ -318,6 +256,7 @@ void motorController::tick()
 		break;
 		case executingInstruction_st:
 		{
+			//continuous PI checking
 			distCheck(); 
 			speedCheck();
 		}
--- motor_control_API/motorController.h
+++ motor_control_API/motorController.h
@@ -13,7 +13,7 @@
 //typedefs
 typedef int16_t mc_distance_t; //in feet
 typedef uint8_t mc_speed_t; //in feet per second
-typedef uint16_t mc_rotation_t; //in degrees
+typedef uint16_t mc_rotation_t; //in degrees per second
 typedef enum { LEFT = 0, RIGHT = 1 } mc_LRDir_t;	// left/right direction
 typedef enum { BACKWARD = 0, FORWARD = 1 } mc_FBDir_t;	// forward/backward direction
 
@@ -27,19 +27,11 @@ typedef enum
 	
 }motorController_st_t;
 
-//available instructions (Should do a type with instruction string, parse within)
+//instructions struct (probably will come from main board)
 typedef struct
 {
 	
 } instruction_t;
-
-enum
-{
-	straightLine_inst = 0,
-	turnRad_inst = 1,
-	turnInPlace_inst = 2,
-	gameControl_inst = 3
-}instruction_e;
 
 class motorController
 {
@@ -55,53 +47,48 @@ public:
 	void turnInPlace(mc_LRDir_t direction, mc_rotation_t degrees);
 
 	bool isAvailable();		//returns availability of motor controller
-	void getInstruction(instruction_t instructionStr);	//gets instruction from i2c, sets instruction received flag
-	void stopGameController();	//lowers gameController flag
+	void getInstruction();	//gets instruction from i2c, sets instruction received flag
+	void stopGameController();	//lowers gameController flag, signaling to stop using game controller
 	void tick();	//standard tick function
 	
 private:
 	//motors
 	motor leftMotor;
 	motor rightMotor;
+	
+	//helper function to stop robot
 	void stopRobot();
 	
 	//encoder functions
-	mc_speed_t getLeftSpeed();
-	mc_speed_t getRightSpeed();
-	mc_distance_t getLeftDistance();
-	mc_distance_t getRightDistance();
+	mc_speed_t getLeftSpeed();	//gets current left speed
+	mc_speed_t getRightSpeed();	//gets current right speed
+	mc_distance_t getLeftDistance();	//gets left distance traveled
+	mc_distance_t getRightDistance();	//gets right distance traveled
 	
-	void updateMotors();
+	void updateMotors();	//updates motors to run at previously adjusted speeds
 	void speedCheck();	//checks if speed needs to be adjusted
 	void distCheck();	//checks if target distance has been accomplished
 	
 	//targets used for checking in execution state
 	mc_speed_t leftTargetSpeed,		//used to maintain correct speed
 			   rightTargetSpeed;		//used to maintain correct speed
-	mc_distance_t targetDistance;	//used to control run time
-	
-	//comm channels
-	SERCOM I2CSlave(SERCOM0);	//for receiving instructions
-	SERCOM I2CMaster(SERCOM1);	//for communication with encoders
-	USBDevice gameController;
-	
-	//instruction data
-	mc_speed_t instSpeed;
-	mc_distance_t instDistance;
-	mc_distance_t instRadius;
-	mc_FBDir_t instFBDir;
-	mc_LRDir_t instLRDir;
-	
+	mc_distance_t targetDistance;	//used to control run time/distance
 	//directions
 	mc_FBDir_t leftDir, rightDir;
 	mc_LRDir_t turnDir;
+		
+	//comm channels
+	SERCOM I2CSlave;	//for receiving instructions from main board
+	SERCOM I2CMaster;	//for communication with encoders
+	USBDevice gameController;	//used for controlling robot with game controller
+	instruction_t nextInstruction;	//instruction to be executed
 	
 	//state machine variables/flags
 	motorController_st_t currState, prevState;
-	bool isAvailable;
-	bool instComplete;
+
+	bool isAvailable;	//availability flag
+	bool instComplete;	//instruction complete flag
 	bool instRecievedFlag;	//indicates if an instruction has been received
-	bool gameControllerFlag;	//indicates if game controller is being used
-	instruction_t nextInstruction;	//instruction to be executed
+	bool gameControllerFlag;	//indicates if game controller is being used for control
 };
 #endif
