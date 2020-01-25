5b9d841e5ff2abbf545f3083a4f28de597ba555e
mxc42 <morgandavidhunter@gmail.com>
2020-01-25 11:43:59 -0700

    init

--- PID/C_files/PID.h
+++ PID/C_files/PID.h
@@ -1,4 +1,18 @@
+#include <stdint.h>
+
 class PID {
     public:
     private:
+	    struct P {
+		    uint8_t m;
+		    uint8_t g;
+		    uint8_t R;
+
+
+	    };
+	    uint16_t vel_d; // desired linear velocity
+	    uint16_t vel; // current linear velocity
+	    uint16_t radius_d; // desired turning radius
+	    uint16_t radius; // current turning radius
+
 };
---
+++ PID/C_files/Params.h
@@ -0,0 +1,6 @@
+class Params {
+	public:
+	private:
+		uint8_t mass;
+		const uint8_t gravity = 9.81;
+};
