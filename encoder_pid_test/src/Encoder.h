0adc8fca3e32aa13cc9caf07dc915b3070fcb316
mxc42 <morgandavidhunter@gmail.com>
2020-02-29 10:40:44 -0700

    added info on how to implement in main

--- encoder_pid_test/src/Encoder.h
+++ encoder_pid_test/src/Encoder.h
@@ -20,3 +20,35 @@ class Encoder {
 
 		//uint16_t num_encoder_ticks = 0;
 };
+
+
+/*
+ *
+ * THE FOLLOWING MUST BE INCLUDED IN MAIN FOR THIS CLASS TO WORK CORRECTLY
+ *
+#define INTERRUPT_PIN_R 2
+#define INTERRUPT_PIN_L 3
+
+Encoder encoder_l;
+Encoder encoder_r;
+
+void enc_right_trigger() {
+	encoder_r.trigger_cnt();
+}
+
+void enc_left_trigger() {
+	encoder_l.trigger_cnt();
+}
+
+void setup() {
+	// Encoder setup -- unfortunately, the way Ardiuno implements interrupts, this can only be done in main.
+	//  it can't be done inside the encoder class. we also need the enc_*_trigger wrappers. :/
+	encoder_l = Encoder();
+	encoder_r = Encoder();
+    pinMode(INTERRUPT_PIN_R, INPUT_PULLUP);
+    pinMode(INTERRUPT_PIN_L, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN_R), enc_right_trigger, RISING);
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN_L), enc_left_trigger, RISING);
+
+}
+*/
