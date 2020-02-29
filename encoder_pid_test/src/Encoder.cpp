735c8f5bc31d4b7e1ec6f7554b326294d0a124e6
mxc42 <morgandavidhunter@gmail.com>
2020-02-29 10:34:40 -0700

    updated encoder function

--- encoder_pid_test/src/Encoder.cpp
+++ encoder_pid_test/src/Encoder.cpp
@@ -1,20 +1,30 @@
 #include "Encoder.h"
 
+// time between reads before we assume the robot is stopped (in ms)
+#define ZERO_VEL_VAL 1000
+
+// constant to convert time into degrees / s
+#define TIME_CONSTANT 10
+
+// default constructor
 Encoder::Encoder() {
-    
 }
 
-uint32_t Encoder::get_time_diff() {
-    return millis() - inter_time;
-}
-
+// returns the current angular velocity in degrees/sec
 uint16_t Encoder::get_speed() {
-    uint16_t interrupts = num_encoder_ticks;
-    num_encoder_ticks = 0;
-
-    return (interrupts << 9) / get_time_diff();
+	// if the last encoder tick was greater than ZERO_VEL_VAL, return 0
+	if ((last_tick_time - millis()) > ZERO_VEL_VAL) {
+		return 0;
+	}
+	
+	// this should return an approximation of the speed
+	return TIME_CONSTANT / (second_to_last_tick_time - last_tick_time)
 }
 
+// interrupt function. this should be attached to the interrupt encoder pin via a wrapper in main
 void Encoder::trigger_cnt() {
-    num_encoder_ticks++;
+	second_to_last_tick_time = last_tick_time;
+	last_tick_time = millis();
+
+	//num_encoder_ticks++; // this is purely for measurement in testing, not needed long term
 }
--- encoder_pid_test/src/Encoder.h
+++ encoder_pid_test/src/Encoder.h
@@ -2,21 +2,21 @@
 #include <Arduino.h>
 
 class Encoder {
-    public:
-    Encoder();
+	public:
+		// constructor
+		Encoder();
 
-    // called every interrupt
-    void trigger_cnt();
+		// called every interrupt
+		void trigger_cnt();
 
-    // returns a representation of the average speed since the last call as a
-    //  factor of the number of interrupts recieved. Also clears
-    //  the number of counts
-    uint16_t get_speed();
+		// returns the current angular velocity in degrees/sec
+		uint16_t get_speed();
 
-    private:
-    uint32_t get_time_diff();
-    
-    uint32_t inter_time = 0;
-    uint32_t time_diff = 0;
-    uint16_t num_encoder_ticks = 0;
+	private:
+		// time of the last encoder edge read
+		uint32_t last_tick_time;
+		// time of the second to last encoder edge read
+		uint32_t second_to_last_tick_time;
+
+		//uint16_t num_encoder_ticks = 0;
 };
